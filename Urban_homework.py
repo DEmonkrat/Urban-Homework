5bac77e9ac13294037bc88fe0db64ea038e542fa
DEmonkrat <manbekov.84@gmail.com>
2024-10-09 21:38:30 +0300

    10.07 Functions.

--- Urban_homework.py
+++ Urban_homework.py
@@ -1,28 +1,27 @@
-calls = 0
+def send_email(message, recipient, *, sender='university.help@gmail.com'):
+    if '@' not in recipient or \
+            '@' not in sender or \
+            (not recipient.endswith('.com') and
+             not recipient.endswith('.ru') and
+             not recipient.endswith('.net')) or \
+            (not sender.endswith('.com') and
+             not sender.endswith('.ru') and
+             not sender.endswith('.net')):
+        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
+    elif sender == recipient:
+        print('Нельзя отправлять самому себе')
+    elif sender == 'university.help@gmail.com':
+        print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}')
+    else:
+        print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}')
 
 
-def count_calls():
-    global calls
-    calls += 1
+mails = [['asdfsdf', 'dima.com', None],
+         ['ljkhlkjh', 'banket@mail', None],
+         ['xzcvxcz', 'dima@mail.ru', 'urban@yandex'],
+         ['zxcvzxcv', 'university.help@gmail.com', ''],
+         ['uyr', 'dima@mail.ru', None],
+         ['uyr', 'dima@mail.ru', 'alina@yandex.com']]
 
-
-def string_info(string):
-    count_calls()
-    return (len(string), string.upper(), string.lower())
-
-
-def is_contains(string, list_of_str):
-    count_calls()
-    return any(string.lower() == str_of_list.lower() for str_of_list in list_of_str)
-
-
-# Вызовем сразу несколько раз string_info
-for i in 'каприз клоун колба колун крона уклон колыбель карта'.split():
-    print(string_info(i))
-print()
-# Возьмем те же слова для is_contains
-for j in 'каприз клоун лопата сережка колыбель стружка орден'.split():
-    print(f'Слово "{j}" находится в списке') if is_contains(j, 'каприз клоун колба колун крона уклон колыбель карта'.split()) else print(f'Слова "{j}" в списке нет')
-print()
-
-print(f'Всего запросов фукций: {calls}')
+for mail in mails:
+    send_email(mail[0], mail[1], sender=mail[2]) if mail[2] else send_email(mail[0], mail[1])
