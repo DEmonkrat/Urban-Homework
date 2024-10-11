b15f9e4ab6e4b1274d96ccee19a0e19aa2775e09
DEmonkrat <manbekov.84@gmail.com>
2024-10-11 17:30:38 +0300

    10.07 Functions. 11.07 Произвольное число параметров

--- Urban_HW_functions.py
+++ Urban_HW_functions.py
@@ -64,3 +64,15 @@ print_params(**values_dict)
 values_list_2 = [False, (1941, 1945)]
 print_params(*values_list_2, 42)
 print('Все работает :)')
+
+
+#Task 4
+#Произвольное число параметров
+def single_root_words(root_word, *other_words):
+  same_words = []
+  for word in other_words:
+    if (root_word.lower() in word.lower()) or (word.lower() in root_word.lower()): same_words.append(word)
+  return same_words
+
+print(single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies'))
+print(single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel'))
