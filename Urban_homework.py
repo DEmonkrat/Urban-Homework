703e5a5c0be899432893617fed0bbc9db6951f22
DEmonkrat <manbekov.84@gmail.com>
2024-10-18 18:44:47 +0300

    Functions. Произвольное число параметров 24.10.18 Работа с markdown, доп задание  по рекурсиям

--- Urban_homework.py
+++ Urban_homework.py
@@ -1,27 +1,48 @@
-def send_email(message, recipient, *, sender='university.help@gmail.com'):
-    if '@' not in recipient or \
-            '@' not in sender or \
-            (not recipient.endswith('.com') and
-             not recipient.endswith('.ru') and
-             not recipient.endswith('.net')) or \
-            (not sender.endswith('.com') and
-             not sender.endswith('.ru') and
-             not sender.endswith('.net')):
-        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
-    elif sender == recipient:
-        print('Нельзя отправлять самому себе')
-    elif sender == 'university.help@gmail.com':
-        print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}')
-    else:
-        print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}')
+import types
+def flat_list_sum(list_):
+  #Если передан список/кортеж, то раскрываем
+  if isinstance(list_, list|tuple):
 
+    if len(list_) == 0:
+      return 0  #Если дошли до конца, вернуть ноль
 
-mails = [['asdfsdf', 'dima.com', None],
-         ['ljkhlkjh', 'banket@mail', None],
-         ['xzcvxcz', 'dima@mail.ru', 'urban@yandex'],
-         ['zxcvzxcv', 'university.help@gmail.com', ''],
-         ['uyr', 'dima@mail.ru', None],
-         ['uyr', 'dima@mail.ru', 'alina@yandex.com']]
+    if isinstance(list_[0], int|float): #Если элемент списка число/булево, возвращаем его и идем дальше
+      return list_[0] + flat_list_sum(list_[1:])
 
-for mail in mails:
-    send_email(mail[0], mail[1], sender=mail[2]) if mail[2] else send_email(mail[0], mail[1])
+    if isinstance(list_[0], str): #Если элемент списка строка, возвращаем ее длину и идем дальше
+      return len(list_[0]) + flat_list_sum(list_[1:])
+
+    if isinstance(list_[0], list|tuple): #Если элемент списка список или кортеж - раскрываем его и смотрим дальше
+      return flat_list_sum(list_[0]) + flat_list_sum(list_[1:])
+
+    if isinstance(list_[0], set): #Если элемент списка множество - перевести в список, раскрыть его и смотрим дальше
+      return flat_list_sum(list(list_[0])) + flat_list_sum(list_[1:])
+
+    if isinstance(list_[0], dict):
+      key_sum, value_sum = 0, 0
+      for key, value in list_[0].items():
+        key_sum += flat_list_sum(key)
+        value_sum += flat_list_sum(value)
+      return key_sum + value_sum + flat_list_sum(list_[1:])
+    else: return 0 #Для типа данных None (если часть списка)
+
+  #Если передано число/строка/словарь, то считаем и выдаем.
+  #Если множетсво, то в словарь и раскрываем
+  if isinstance(list_, int|float): #Если элемент число/булево, возвращаем его
+    return list_
+
+  if isinstance(list_, str): #Если элемент строка, возвращаем ее длину
+    return len(list_)
+
+  if isinstance(list_, dict): #Если словарь, то считаем его
+    key_sum, value_sum = 0, 0
+    for key, value in list_.items():
+      key_sum += flat_list_sum(key)
+      value_sum += flat_list_sum(value)
+    return key_sum + value_sum
+
+  if isinstance(list_, set): #Если элемент множество - перевести в список, раскрыть его и смотрим дальше
+    return flat_list_sum(list(list_))
+  else: return 0 #Для типа данных None
+
+print(flat_list_sum([1,2,[3,4]]))
