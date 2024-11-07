d2764a90171fcce4fefd932d91b1ce2a0101016f
DEmonkrat <manbekov.84@gmail.com>
2024-11-07 14:17:39 +0300

    24.10.08 Dop zadaniye po chislam 24.10.11 Add file: ...curentHW, add Task 3 24.10.21 Task 5 Rekursiya (dop zadaniye) 24.10.23 add module_4_1, fake_math, true_math 24.10.31 Add module_5_1 24.11.02 Add module_5_2, module_5_3

--- module_5_2.py
+++ module_5_2.py
@@ -8,6 +8,7 @@ class House:
 
     def __str__(self):
         return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
+
     def go_to(self, new_floor):
         if (new_floor < 1) or (new_floor > self.number_of_floors):
             print('Такого этажа не существует')
