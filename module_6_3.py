1d6b7c7446aea855b9ad2655d37f7655201821dd
DEmonkrat <manbekov.84@gmail.com>
2024-11-14 10:29:32 +0300

    24.10.08 Dop zadaniye po chislam 24.10.11 Add file: ...curentHW, add Task 3 24.10.21 Task 5 Rekursiya (dop zadaniye) 24.10.23 add module_4_1, fake_math, true_math 24.10.31 Add module_5_1 24.11.02 Add module_5_2, module_5_3 24.11.12 Add module_5_hard 24.11.12 Add module_6_1 24.11.13 Add module_6_2 24.11.14 Add module_6_3

---
+++ module_6_3.py
@@ -0,0 +1,83 @@
+# ____________Animal___________________
+class Animal:
+    live = True
+    sound = None
+    _DEGREE_OF_DANGER = 0
+
+    def __init__(self, speed):
+        self._coords = [0, 0, 0]
+        self.speed = speed
+
+    def move(self, dx, dy, dz):
+        if (self._coords[2] + dz) < 0:
+            print('It\'s too deep, i can\'t dive :(')
+        else:
+            self._coords[0] += dx * self.speed
+            self._coords[1] += dy * self.speed
+            self._coords[2] += dz * self.speed
+
+    def get_coords(self):
+        print(f'X: {self._coords[0]}, Y: {self._coords[1]}, Z: {self._coords[2]}')
+
+    def attack(self):
+        if self._DEGREE_OF_DANGER <= 5:
+            print('Sorry, i\'m peaceful :)')
+        else:
+            print('Be careful, i\'m attacking you 0_0')
+
+    def speak(self):
+        print(self.sound)
+
+
+# ____________Bird___________________
+class Bird(Animal):
+    beak = True
+    sound = 'tweat'
+
+    def lay_eggs(self):
+        from random import randint
+        print(f'Here are(is) {randint(1, 4)} eggs for you')
+
+
+# ____________AquaticAnimal___________________
+class AquaticAnimal(Animal):
+    _DEGREE_OF_DANGER = 3
+
+    def dive_in(self, dz):
+        self._coords[2] -= abs(dz) * self.speed // 2
+
+
+# ____________PoisonousAnimal___________________
+class PoisonousAnimal(Animal):
+    _DEGREE_OF_DANGER = 8
+    sound = 'RRRrrrRRRRRrrrrr'
+
+
+# ____________Duckbill___________________
+class Duckbill(PoisonousAnimal, AquaticAnimal, Bird):
+    sound = 'Click-click-click'
+
+
+b1 = Bird(5)
+b1.attack()
+b1.lay_eggs()
+b1.move(1, 2, 3)
+b1.get_coords()
+b1.move(3, 4, -10)
+b1.speak()
+print(b1.__dict__)
+print()
+
+db = Duckbill(10)
+print(db.live)
+print(db.beak)
+
+db.speak()
+db.attack()
+
+db.move(1, 2, 3)
+db.get_coords()
+db.dive_in(6)
+db.get_coords()
+
+db.lay_eggs()
