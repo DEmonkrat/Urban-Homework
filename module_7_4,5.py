2b3636d0be90f105394c8adc7d9eb5304c3dcd4c
DEmonkrat <manbekov.84@gmail.com>
2024-11-22 20:45:58 +0300

    24.10.08 Dop zadaniye po chislam 24.10.11 Add file: ...curentHW, add Task 3 24.10.21 Task 5 Rekursiya (dop zadaniye) 24.10.23 add module_4_1, fake_math, true_math 24.10.31 Add module_5_1 24.11.02 Add module_5_2, module_5_3 24.11.12 Add module_5_hard 24.11.22 Add module_7_4,5.py

--- module_7_4,5.py
+++ module_7_4,5.py
@@ -1,3 +1,6 @@
+import os
+from traceback import print_tb
+
 # Форматирование строк (НИЖЕ Файлы в операционной системе)
 # Использование %
 team1_num = 5
@@ -26,6 +29,63 @@ print(f'Результат битвы: {challenge_result}!')
 tasks_total = sum([score_1, score_2])
 time_avg = sum([team1_time, team2_time])/tasks_total
 print(f'Сегодня было решено {tasks_total} задачи, в среднем по {time_avg:.2f} секунды на задачу!')
+print(end='\n\n\n')
 
+# Следующее домашнее задание !!!
 # Файлы в операционной системе
-print(chr(356))
+def print_size(size, name=''):
+    print('Название файла: ', name)
+    if size//10**3 < 1:
+        print(f'Размер файла: {size} байт')
+    elif size//10**6 < 1:
+        print(f'Размер файла: {size//10**3:.2f} Кбайт')
+    else:
+        print(f'Размер файла: {size//10**6:.2f} Мб')
+    print()
+
+
+print('Метод OS.WALK')
+directory = '.'
+for cur_dir, dirs, files in os.walk(directory):
+    print(f'Текущий каталог: {os.path.join(os.getcwd(), cur_dir[2:])}')
+    print(f'Подкаталоги: ', end='')
+    if not dirs:
+        print('пусто')
+    elif len(dirs) < 8:
+        print(*dirs, sep=', ')
+    else:
+        print(*dirs[:5],  ' ... ', dirs[-1] , sep=', ', end='')
+        print(' | Всего каталогов: ', len(dirs))
+    print('Файлы: ', end='')
+    if not files:
+        print('пусто', end='\n\n')
+    elif len(files) < 8:
+        print(*files, sep=', ', end='\n\n')
+    else:
+        print(*files[:5],  ' ... ', files[-1] , sep=', ', end='')
+        print(' | Всего файлов: ', len(files), end='\n\n')
+
+import time
+print('Метод os.path.getmtime')
+file_name = 'module_4_1.py'
+file_time = time.localtime(os.path.getmtime(file_name))
+print(f'Время последней модификации файла "{file_name}":')
+print(*file_time[:3], sep='-', end=' ')
+print(*file_time[3:6], sep=':')
+file_size = os.path.getsize(file_name)
+with open('test_size.txt', 'w') as f:
+    f.write('test_size'*10**3)
+
+print_size(file_size, file_name)
+print_size(os.path.getsize('test_size.txt'), 'test_size.txt')
+print()
+print('Теперь увеличим размер файла')
+with open('test_size.txt', 'w') as f:
+    f.write('test_size'*10**6)
+print_size(os.path.getsize('test_size.txt'), 'test_size.txt')
+# Между просим винда пишет 8.790 Мб
+# Удалим наш БОЛЬШОЙ файл
+os.remove('test_size.txt')
+
+print('Метод os.path.dirname')
+print(os.path.dirname(r'Module_7_3_files\text.txt'))
