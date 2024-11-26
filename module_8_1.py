e526dd3757a8cdacc5dcdd294bc51805b3cda670
DEmonkrat <manbekov.84@gmail.com>
2024-11-26 14:51:28 +0300

    24.11.22 Add module_7_4,5.py 24.11.26 Add module_8_1.py

--- module_8_1.py
+++ module_8_1.py
@@ -1,13 +1,15 @@
 from decimal import *
+
+
 def add_everything_up(a, b):
     try:
-        a + b
-        return Decimal(str(a)) + Decimal(str(b))
+        a + b   # Пробуем сложить, если не получается, значит есть строки ...
+        return Decimal(str(a)) + Decimal(str(b))    # .. если получается, то используем Decimal для точности
     except TypeError:
         print('Не все элементы числа, поэтому используем конкатенацию: ')
         return str(a) + str(b)
 
+
 print(add_everything_up(123.456, 'строка'))
 print(add_everything_up('яблоко', 4215))
 print(add_everything_up(123.456, 7))
-# print(Decimal('123.456'))
