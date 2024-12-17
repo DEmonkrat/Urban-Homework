646a128e7dc26eb2d4e20849d09a36ac178a5474
DEmonkrat <manbekov.84@gmail.com>
2024-12-17 10:39:09 +0300

    24.11.22 Add module_7_4,5.py 24.11.26 Add module_8_1.py 24.11.28 Add module_8_2.py 24.12.03 Add module_8_3.py 24.12.05 Add module_9_1.py 24.12.05 Add module_9_2.py 24.12.09 Add module_9_3.py, module_9_4.py, module_9_5.py 24.12.10 Correction module_9_5.py 24.12.10 ATTENTION !!! Add folder for modules; add module_9_6.py, module_9_7.py 24.12.16 Add module_10_1.py

--- Mod_10/module_10_3.py
+++ Mod_10/module_10_3.py
@@ -1,40 +1,61 @@
 import threading, time
 from random import randint
 
+
 class Bank():
     def __init__(self):
         self.balance = 0
-        self.lock = threading.Lock()
+        self.lock = threading.Lock()  # Экземпляр класса Лок для блокировки снятия
+        self.b_lock = threading.Lock()  # Экземпляр класса Лок для блокировки операций со счетами/балансом
 
     def deposit(self):
         for _ in range(100):
-            withdraw = randint(50, 500)
-            self.balance += withdraw
-            print(f'Пополнение на: {withdraw}. Баланс: {self.balance}')
-            if self.balance >= 500 and self.lock.locked():
+            refill = randint(50, 100)   # Специально поменьше сделал, чтобы точно срабатывала блокировка
+            self.__bal_change(refill)
+            print(f'Пополнение на: {refill}. Баланс: {self.__bal_check()}')
+            if self.__bal_check() >= 500 and self.lock.locked():
                 self.lock.release()
                 print('Поток разблокирован')
             time.sleep(0.001)
+        if self.lock.locked():
+            self.lock.release() # Отпускаем замок, если исчерпали range и замок заблокирован
+            print('Поток разблокирован')
+            print('Поток пополнения иссяк (((')
 
     def take(self):
         for _ in range(100):
             removal = randint(50, 500)
             print(f'Запрос на снятие: {removal}')
-            if (self.balance - removal) >= 0:
-                self.balance -= removal
-                print(f'Снятие: {removal}. Баланс: {self.balance}')
+            if self.lock.locked():
+                print(threading.current_thread())
+                self.lock.acquire()
+            else:
+                print('Снятие. Замок открыт')
+
+            if (self.__bal_check() - removal) >= 0:
+                self.__bal_change(-removal)
+                print(f'Снятие: {removal}. Баланс: {self.__bal_check()}')
             else:
                 print('Запрос отклонён, недостаточно средств')
-                self.lock.acquire()
-                print('Поток заблокирован')
+                if threading.active_count() > 2:
+                    self.lock.acquire()
+                    print('Поток заблокирован. Потоков:', threading.active_count())
             time.sleep(0.001)
+        print('Поток снятия иссяк !!!')
 
+    def __bal_check(self):
+        with self.b_lock:
+            return self.balance
+
+    def __bal_change(self, by):
+        with self.b_lock:
+            self.balance += by
 
 
 bk = Bank()
 
-th1 = threading.Thread(target=Bank.deposit, args=(bk,))
-th2 = threading.Thread(target=Bank.take, args=(bk,))
+th1 = threading.Thread(target=Bank.deposit, args=(bk,), name='refill')
+th2 = threading.Thread(target=Bank.take, args=(bk,), name='removal')
 
 th1.start()
 th2.start()
