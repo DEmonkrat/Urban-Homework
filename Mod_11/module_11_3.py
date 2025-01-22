0c8cebeaeaa2fa93e82a5f9f2e72347676d3081d
DEmonkrat <manbekov.84@gmail.com>
2025-01-22 11:31:38 +0300

    25.01.21 Add mod_11_3

--- Mod_11/module_11_3.py
+++ Mod_11/module_11_3.py
@@ -1,6 +1,8 @@
 from pprint import pprint
+
+
 class My_class:
-    def __init__(self, a ,b):
+    def __init__(self, a, b):
         self.a = a
         self.b = b
 
@@ -16,5 +18,12 @@ def introspection_info(obj):
     return intr_res
 
 
+class_info = introspection_info(My_class)
+method_info = introspection_info(My_class.print_ab)
 number_info = introspection_info(42)
+print('Число')
 pprint(number_info)
+print('Класс')
+pprint(class_info)
+print('Метод класса')
+pprint(method_info)
