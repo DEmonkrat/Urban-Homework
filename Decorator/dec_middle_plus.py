3bca6a0cfbe097352777f26b1ab56bf071d2f791
DEmonkrat <manbekov.84@gmail.com>
2026-03-03 14:39:16 +0300

    Learning decors

--- Decorator/dec_middle_plus.py
+++ Decorator/dec_middle_plus.py
@@ -1,9 +1,19 @@
 import json
 
-j_dump = json.dumps({'key':1, 'val':2, 'dicti': {"li":1, 'lu':2}})
-print(type(j_dump))
-print(j_dump)
-i = 1
-for symb in j_dump:
-    print(f"Symb number: {i}; value = {symb}")
-    i += 1
+def to_json(func):
+    def wrapper(*args):
+        with open('data.json', 'w', encoding='utf-8') as file:
+            json.dump(func(*args), file, ensure_ascii=False, indent=4)
+        return json.dumps(func(*args), ensure_ascii=False, indent=4)
+
+    return wrapper
+
+@to_json
+def get_user_data(user_id):
+    return {"id": user_id, "name": "John Doe", "permissions": ["read", "write"]\
+            , "lib_acc": {"1_book": "Maugli", "2_book": "Karlson"}}
+
+json_string = get_user_data(123)
+print(json_string)
+# Выведет: {"id": 123, "name": "John Doe", "permissions": ["read", "write"]}
+print(type(json_string)) # Выведет: <class 'str'>
